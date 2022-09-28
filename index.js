70d19434fdf70b36bbb05ee07ded57ac49284d99
Jeff Bayekula <jeffbayekula@gmail.com>
2022-09-29 00:53:05 +0300

    Adding starting files

---
+++ index.js
@@ -0,0 +1,47 @@
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+saveEl.textContent = "Total attendees are: ";
+let count = 0;
+let countCheck = [];
+
+function increment() {
+  count += 1;
+  countEl.textContent = count;
+}
+
+function decrement() {
+  count -= 1;
+  if (count < 0) {
+    countEl.textContent = countEl.textContent = "Negative numbers not allowed.";
+  } else {
+    countEl.textContent = count;
+  }
+}
+
+function save() {
+  let countStr = " " + count + " - ";
+  if (count <= 0) {
+    saveEl.textContent;
+  } else {
+    saveEl.textContent += countStr;
+    countCheck.push(count);
+    countEl.textContent = 0;
+    count = 0;
+  }
+  console.log(countCheck);
+}
+
+function resetAll() {
+  countEl.textContent = 0;
+  saveEl.textContent = "Total attendees are: ";
+  count = 0;
+  countCheck = [];
+}
+
+function total() {
+  saveEl.textContent = "Total attendees are: " + countCheck.reduce(myFunc);
+  function myFunc(total, num) {
+    return total + num;
+  }
+  // countCheck = [];
+}
